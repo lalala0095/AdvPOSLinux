e57443195a2f6fa11008fa33e6c6f494764be5e4
lemuel-cljteam <lemuelt@cljteam.com>
2024-12-06 07:02:14 +0800

    added sitemap for google search console

--- app.py
+++ app.py
@@ -9,18 +9,17 @@ app = create_app()
 def generate_sitemap(app, domain):
     sitemap = ['<?xml version="1.0" encoding="UTF-8"?>']
     sitemap.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
-    
+
     # Automatically retrieve routes from Flask
-    with app.test_request_context():
-        for rule in app.url_map.iter_rules():
-            if "GET" in rule.methods and not rule.arguments:
-                url = url_for(rule.endpoint, _external=True).replace("http://localhost", domain)
-                sitemap.append('<url>')
-                sitemap.append(f'<loc>{url}</loc>')
-                sitemap.append(f'<lastmod>{datetime.now().strftime("%Y-%m-%d")}</lastmod>')
-                sitemap.append('<changefreq>daily</changefreq>')
-                sitemap.append('<priority>0.5</priority>')
-                sitemap.append('</url>')
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and not rule.arguments:
+            url = url_for(rule.endpoint, _external=True).replace("http://127.0.0.1:5000", domain)
+            sitemap.append('<url>')
+            sitemap.append(f'<loc>{url}</loc>')
+            sitemap.append(f'<lastmod>{datetime.now().strftime("%Y-%m-%d")}</lastmod>')
+            sitemap.append('<changefreq>daily</changefreq>')
+            sitemap.append('<priority>0.5</priority>')
+            sitemap.append('</url>')
 
     sitemap.append('</urlset>')
     return ''.join(sitemap)
